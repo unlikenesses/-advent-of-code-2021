ae96ac863391fc95ce9ff02dd5de3d59caeaf036
Unlikenesses <unlikenesses@gmail.com>
2021-12-07 23:59:19 +0100

    Day 1

---
+++ 1/solution.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/unlikenesses/utils"
+)
+
+func main() {
+	lines := utils.ReadInput()
+
+	part1 := partOne(lines)
+	fmt.Println(part1)
+
+	part2 := partTwo(lines)
+	fmt.Println(part2)
+}
+
+func partOne(lines []string) int {
+	prev := 0
+	total := 0
+	for i, line := range lines {
+		depth, _ := strconv.Atoi(line)
+		if i == 0 {
+			prev = depth
+			continue
+		}
+		if depth > prev {
+			total++
+		}
+		prev = depth
+	}
+	return total
+}
+
+func partTwo(lines []string) int {
+	window_sums := getWindowSums(lines)
+	prev := 0
+	total := 0
+	for i, sum := range window_sums {
+		if i == 0 {
+			prev = sum
+			continue
+		}
+		if sum > prev {
+			total++
+		}
+		prev = sum
+	}
+
+	return total
+}
+
+func getWindowSums(lines []string) []int {
+	size := len(lines)
+	var windows []int
+	for i := 0; i < size-2; i++ {
+		depth1, _ := strconv.Atoi(lines[i])
+		depth2, _ := strconv.Atoi(lines[i+1])
+		depth3, _ := strconv.Atoi(lines[i+2])
+		window := []int{depth1, depth2, depth3}
+		windowSum := sumArray(window)
+		windows = append(windows, windowSum)
+	}
+
+	return windows
+}
+
+func sumArray(arr []int) int {
+	sum := 0
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+	}
+
+	return sum
+}
